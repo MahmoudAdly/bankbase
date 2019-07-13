e7dbc60c3da69ac54dbc1d8ccb6b62c1527b9b18
Mahmoud Adly <mahmoud.adly@gmail.com>
2019-07-13 19:08:49 +0200

    Integrate MaterializeCSS and list all user transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
 
   validates :name, length: { in: 2..24 }
 
   has_many :sent_transactions, class_name: "Transaction", foreign_key: "sender_id"
   has_many :received_transactions, class_name: "Transaction", foreign_key: "receiver_id"
+
+  # TODO: Move out of the model to a separate helper or service.
+  def all_transactions
+    Transaction.where(sender: self).or(Transaction.where(receiver: self))
+  end
 end
