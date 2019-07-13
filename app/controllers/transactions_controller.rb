c98b6562709b531c5ff8875372ad27ee415dbe4e
Mahmoud Adly <mahmoud.adly@gmail.com>
2019-07-13 20:29:43 +0200

    Add a transaction creation feature
    - Create a new view for new transaction.
    - Update the routes and transactions_controller.
    - Use the dummy service for exchange rate.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,5 +2,34 @@ class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @all_user_transactions = current_user.all_transactions
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    # TODO:
+    # - Move that logic to a new service.
+    # - Schedule a job to process the new transaction.
+    @transaction.sender = current_user
+    @transaction.exchange_rate = FetchExchangeRate.new(@transaction.source_currency,
+                                                       @transaction.target_currency).call
+
+    if @transaction.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private def transaction_params
+    params.require(:transaction).permit(:receiver_id,
+                                        :source_currency,
+                                        :target_currency,
+                                        :amount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'transactions/index'
+  resources :transactions, only: [:new, :create]
 end
